54982d28dddc1e17819ee5356f9f51dfd684efcb
jcmagal <joaoc.magalhaes97@gmail.com>
2025-02-10 17:47:22 -0400

    commit inicial

---
+++ src/main/java/com/magal/agendamentonotificacaoapi/AgendamentoNotificacaoApiApplication.java
@@ -0,0 +1,13 @@
+package com.magal.agendamentonotificacaoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoNotificacaoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgendamentoNotificacaoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/magal/agendamentonotificacaoapi/AgendamentoNotificacaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magal.agendamentonotificacaoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoNotificacaoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
